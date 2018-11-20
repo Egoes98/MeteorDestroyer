bf2d90e671ef526a4ad31702b39561003de29eee
Egoitz <egoes98@gmail.com>
2018-11-20 20:12:03 +0100

    Actualizacion

---
+++ Assets/Scripts/BunkerCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunkerCollision : MonoBehaviour {
+
+    public float damage;
+    public float resistance;
+    public float nearlyDestroyed;
+    public float destroyed;
+
+    public Sprite state1;
+    public Sprite state2;
+    public SpriteRenderer bunker;
+
+    // Update is called once per frame
+    void Update () {
+        if (resistance == nearlyDestroyed)
+        {
+            //Change to the second state sprite
+            bunker.sprite = state1;
+        }
+        if (resistance == destroyed)
+        {
+            //Change to the destroyed state
+            bunker.sprite = state2;
+            //Send message to GM to show that the building is detroyed
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        resistance -= damage;
+        Destroy(collision.gameObject,0);
+    }
+}
---
+++ Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Enter");
+        Destroy(collision.gameObject,0);
+    }
+}
