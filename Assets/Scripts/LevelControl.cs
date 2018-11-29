cd3b4463d64e3c0b0c49d259ce049967bfe9fe14
Egoitz <egoes98@gmail.com>
2018-11-29 09:20:14 +0100

    Rounds
    las rondas ahora funcionan correctamente

--- Assets/Scripts/LevelControl.cs
+++ Assets/Scripts/LevelControl.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class LevelControl : MonoBehaviour {
 
     public Canvas canvas;
+    public bool runTimer;
 
 	// Use this for initialization
 	void Start () {
         canvas.GetComponent<Canvas>().enabled = false;
+        runTimer = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        InvokeRepeating("UpgradeMenu", 30.0f, 30.0f);
         if (Time.timeScale == 1)
         {
             canvas.GetComponent<Canvas>().enabled = false;
+        }
+        else
+        {
+            return;
+        }
+        if (runTimer)
+        {
+            StartCoroutine("UpgradeMenu");
         }
     }
 
@@ -25,10 +34,13 @@ public class LevelControl : MonoBehaviour {
         Debug.Log("Next round");
         MeteorSpawn.fallSpeed += 2;
         Time.timeScale = 1;
+        runTimer = true;
         canvas.GetComponent<Canvas>().enabled = false;
     }
-    public void UpgradeMenu()
+    public IEnumerator UpgradeMenu()
     {
+        runTimer = false;
+        yield return new WaitForSeconds(30);
         Time.timeScale = 0;
         canvas.GetComponent<Canvas>().enabled = true;
     }
